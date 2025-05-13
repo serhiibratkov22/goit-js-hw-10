aa3e0ab2d9fc70eb8d56c2045dd6b9bf2bacdce3
Serhii <sir3720@gmail.com>
2025-05-13 17:05:21 +0300

    add snackbar js html

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,39 @@
-console.log('Snackbar module loaded');
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delay = Number(form.elements.delay.value);
+  const state = form.elements.state.value;
+
+  createPromise(delay, state)
+    .then(ms => {
+      iziToast.success({
+        message: `✅ Fulfilled promise in ${ms}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(ms => {
+      iziToast.error({
+        message: `❌ Rejected promise in ${ms}ms`,
+        position: 'topRight',
+      });
+    });
+
+  form.reset();
+});
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
